4cc37886283495ec6bee41d7d1f5df2734f04c57
TheCre8tor <cre8tor.alexander@gmail.com>
2022-11-01 19:05:54 +0100

    Tested the match arms

--- example/ruqe_example.dart
+++ example/ruqe_example.dart
@@ -3,17 +3,17 @@ import 'package:ruqe/ruqe.dart';
 void main() {
   var trigger = triggerError();
 
-  var value = trigger.match<int?>(
-    ok: (value) => value,
-    err: (e) => 0,
+  trigger.match(
+    ok: (value) => print(value),
+    err: (e) => print(e),
   );
 
-  print(value);
+  // print(value);
 }
 
 Result<int, String> triggerError() {
   try {
-    var value = int.parse("%65");
+    var value = int.parse("65");
     return Ok(value);
   } catch (err) {
     return Err("Value is none");
