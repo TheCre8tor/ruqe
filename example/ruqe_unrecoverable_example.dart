9d57267501446770abd7e0898d0f33551aab5777
TheCre8tor <cre8tor.alexander@gmail.com>
2022-11-03 15:50:27 +0100

    creating documentation for unrecoverable errors

--- example/ruqe_unrecoverable_example.dart
+++ example/ruqe_unrecoverable_example.dart
@@ -3,19 +3,22 @@ import 'package:ruqe/ruqe.dart';
 typedef ListMap = List<Map<String, String>>;
 typedef AppError = String;
 
-void main() {
-  var data = [
-    {"first_name": "Tunbnad", "last_name": "Smart"},
-    {"first_name": "Lambo", "last_name": "Turnner"}
-  ];
+var data = [
+  {"first_name": "Tunbnad", "last_name": "Smart"},
+  {"first_name": "Lambo", "last_name": "Turnner"}
+];
 
+void main() {
   var firstNames1 = getFirstName(data);
   print(firstNames1.unwrap()); // [Tunbnad, Lambo]
 
+  /// Unwrapping a value of [Option] that is [None]
+  /// will trigger a [Panic] and terminate the program.
   var firstNames2 = getFirstName([]);
   firstNames2.unwrap(); // panic with `[error]: an error occured!`
 }
 
+/// Returns a [Result] that is [Err] if [ListMap] is empty.
 Result<List<String?>, AppError> getFirstName(ListMap data) {
   if (data.isNotEmpty) {
     return Ok(data.map((user) => user["first_name"]).toList());
