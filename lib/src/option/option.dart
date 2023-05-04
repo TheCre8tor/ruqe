6a89e047e078d7f694316f213ee08fe4a51328ae
Alexander Nitiola <cre8tor.alexander@gmail.com>
2023-05-04 18:12:59 +0100

    Changed Option<T> type unwrap behaviour

--- lib/src/option/option.dart
+++ lib/src/option/option.dart
@@ -17,22 +17,34 @@ class Some<T> extends Option<T> {
   const Some(T? value) : super(value);
 
   @override
-  List<Object?> get props => [_value];
-
-  @override
   T unwrap() {
     return _value!;
   }
+
+  @override
+  List<Object?> get props => [_value];
 }
 
 class None<T> extends Option<T> {
   const None();
 
+  /// Note: In the original Rust implementation of the unwrap API,
+  /// When unwrap() is called on a result of a None value, the application
+  /// panic's. The same use-case can't be applied to Dart/Flutter application
+  /// because it mess up the UI.
+  ///
+  /// Solution: When unwrap is called on a result of a None value, None<void> is
+  /// returned instead of throwing an exception of Panic<None<T>>
+  ///
+  /// @override
+  /// T unwrap() {
+  ///   throw Panic<None<T>>(
+  ///     Some("panicked at 'called `Option::unwrap()` on a `None` value'"),
+  ///   );
+  /// }
   @override
   T unwrap() {
-    throw Panic<None<T>>(
-      Some("panicked at 'called `Option::unwrap()` on a `None` value'"),
-    );
+    return None<void>() as T;
   }
 
   @override
